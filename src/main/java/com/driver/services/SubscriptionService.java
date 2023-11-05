1f4f3c0f0ae167ce7fdef99901d054e0c26fc419
purshottam <pankajpatidar9521@gmail.com>
2023-11-05 09:46:52 +0530

    commit

--- src/main/java/com/driver/services/SubscriptionService.java
+++ src/main/java/com/driver/services/SubscriptionService.java
@@ -25,8 +25,28 @@ public class SubscriptionService {
     public Integer buySubscription(SubscriptionEntryDto subscriptionEntryDto){
 
         //Save The subscription Object into the Db and return the total Amount that user has to pay
+        // finding user
+        User user = userRepository.findById(subscriptionEntryDto.getUserId()).get();
 
-        return null;
+        //find total amount paid for subscription
+        int totalAmount=0;
+        if(subscriptionEntryDto.getSubscriptionType().equals(SubscriptionType.BASIC)){
+            totalAmount=500+(subscriptionEntryDto.getNoOfScreensRequired()*250);
+        }else if(subscriptionEntryDto.getSubscriptionType().equals(SubscriptionType.PRO)){
+            totalAmount=800+(subscriptionEntryDto.getNoOfScreensRequired()*250);
+        }else{
+            totalAmount=1000+(subscriptionEntryDto.getNoOfScreensRequired()*350);
+        }
+
+
+        Subscription subscription =new Subscription();
+                subscription.setSubscriptionType(subscriptionEntryDto.getSubscriptionType());
+                subscription.setNoOfScreensSubscribed(subscriptionEntryDto.getNoOfScreensRequired());
+                subscription.setUser(user);
+                subscription.setTotalAmountPaid(totalAmount);
+
+        user.setSubscription(subscription);
+        return subscriptionRepository.save(subscription).getId();
     }
 
     public Integer upgradeSubscription(Integer userId)throws Exception{
@@ -35,15 +55,50 @@ public class SubscriptionService {
         //In all other cases just try to upgrade the subscription and tell the difference of price that user has to pay
         //update the subscription in the repository
 
-        return null;
+
+        //find the user;
+        User user=userRepository.findById(userId).get();
+        //find the Subscription
+        Subscription currSubscription=user.getSubscription();
+        //current subscription type
+        SubscriptionType currSubscriptionType=currSubscription.getSubscriptionType();
+
+        if(currSubscriptionType.equals(SubscriptionType.ELITE)){
+            throw new Exception("Already the best Subscription");
+        }
+
+        int changeInPrice=0;
+        int totalPrice=0;
+        if(currSubscriptionType.equals(SubscriptionType.BASIC)){
+            currSubscription.setSubscriptionType(SubscriptionType.PRO);
+            int currentPrice=currSubscription.getTotalAmountPaid();
+            totalPrice=800+currSubscription.getNoOfScreensSubscribed()*250;
+            currSubscription.setTotalAmountPaid(totalPrice);
+            changeInPrice=totalPrice-currentPrice;
+        }else if(currSubscriptionType.equals(SubscriptionType.PRO)){
+            currSubscription.setSubscriptionType(SubscriptionType.ELITE);
+            int currentPrice=currSubscription.getTotalAmountPaid();
+            totalPrice=1000+currSubscription.getNoOfScreensSubscribed()*350;
+            currSubscription.setTotalAmountPaid(totalPrice);
+            changeInPrice=totalPrice-currentPrice;
+        }
+
+        subscriptionRepository.save(currSubscription);
+
+        return changeInPrice;
     }
 
     public Integer calculateTotalRevenueOfHotstar(){
 
         //We need to find out total Revenue of hotstar : from all the subscriptions combined
         //Hint is to use findAll function from the SubscriptionDb
+        List<Subscription> allSubscriptions = subscriptionRepository.findAll();
+        int totalRevenue=0;
+        for(Subscription subscription : allSubscriptions){
+            totalRevenue+=subscription.getTotalAmountPaid();
+        }
 
-        return null;
+        return totalRevenue;
     }
 
 }
--- src/main/java/com/driver/services/WebSeriesService.java
+++ src/main/java/com/driver/services/WebSeriesService.java
@@ -8,6 +8,8 @@ import com.driver.repository.WebSeriesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class WebSeriesService {
 
@@ -24,7 +26,39 @@ public class WebSeriesService {
         //use function written in Repository Layer for the same
         //Dont forget to save the production and webseries Repo
 
-        return null;
-    }
+        //checking that seriesName is present or not
+       WebSeries webSeries1=webSeriesRepository.findBySeriesName(webSeriesEntryDto.getSeriesName());
+       if(webSeries1!=null){
+           throw new Exception("Series is already present");
+       }
 
+        //updating the rating in production house;
+        ProductionHouse productionHouse=updateProductionHouse(webSeriesEntryDto.getProductionHouseId(),webSeriesEntryDto.getRating());
+
+        //creating webSeries
+        WebSeries webSeries=new WebSeries();
+              webSeries.setSeriesName(webSeriesEntryDto.getSeriesName());
+              webSeries.setAgeLimit(webSeriesEntryDto.getAgeLimit());
+              webSeries.setSubscriptionType(webSeriesEntryDto.getSubscriptionType());
+              webSeries.setRating(webSeriesEntryDto.getRating());
+              webSeries.setProductionHouse(productionHouse);
+
+        //setting FR's
+        productionHouse.getWebSeriesList().add(webSeries);
+        WebSeries currWeb=webSeriesRepository.save(webSeries);
+        return currWeb.getId();
+
+    }
+    public ProductionHouse updateProductionHouse(Integer productionHouseId,double rating){
+        ProductionHouse currProductionHouse=productionHouseRepository.findById(productionHouseId).get();
+        List<WebSeries> listOfWebSeries=currProductionHouse.getWebSeriesList();
+        double sum=rating;
+        for(WebSeries web: listOfWebSeries){
+            sum+=web.getRating();
+        }
+        int totalWeb=listOfWebSeries.size()+1;
+        double avgRating=sum/totalWeb;
+        currProductionHouse.setRatings(avgRating);
+        return productionHouseRepository.save(currProductionHouse);
+    }
 }
